e8d086f752a02dc9247710d9a97dfb0c3f8ac387
mancle@naver.com <mancle@naver.com>
2019-03-25 21:05:28 +0900

    new

--- makecode.py
+++ makecode.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = pd.read_csv('from.csv', dtype=str)
+data = pd.read_csv('class.csv', dtype=str)
 
-with open('from.json','w',encoding='utf-8') as file:
+with open('class.json','w',encoding='utf-8') as file:
     data.to_json(file, force_ascii=False, orient='table')
