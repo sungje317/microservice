252e2e0ca9d60182370ff7418cea95acc26fef1e
mancle@naver.com <mancle@naver.com>
2019-04-15 20:30:51 +0900

    update

--- makecode.py
+++ makecode.py
@@ -24,3 +24,10 @@ data = pd.read_csv('poll.csv', dtype=str)
 
 with open('poll.json','w',encoding='utf-8') as file:
     data.to_json(file, force_ascii=False, orient='table')
+
+data = pd.read_csv('monthly.csv', dtype=str)
+data = data.sort_values(["Date"],ascending=True)
+data = data.reset_index(drop=True)
+
+with open('monthly.json','w',encoding='utf-8') as file:
+    data.to_json(file, force_ascii=False, orient='table')
